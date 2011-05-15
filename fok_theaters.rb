2c1992677aed1ab9c05ea7ccadf6b37bb092578f
Jeffrey Hicks <jhicks@cteh.com>
2011-05-15 11:55:12 -0500

    theaters and google api

--- fok_theaters.rb
+++ fok_theaters.rb
@@ -1,10 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'theaters.rb'
 
-
-THEATERS = open("theaters.txt","r").read().split("\n").map{|x| {:name=>x.split("\t")[0], :latitude=>x.split("\t")[1], :longitude=>x.split("\t")[2]}}.select {|t|not (t[:latitude]==nil or t[:latitude]=='')}
-
-GOOGLE_API_CODE = 'ABQIAAAA9OOBC0JIUjp2D8RWTK8KehT2yXp_ZAY8_ufC3CFXhHIE1NvwkxS-XTc7wgiqy8tNe92hPtXqqKOcWg' # localhost
+# LOCAL HOST
+# GOOGLE_API_CODE = 'ABQIAAAA9OOBC0JIUjp2D8RWTK8KehT2yXp_ZAY8_ufC3CFXhHIE1NvwkxS-XTc7wgiqy8tNe92hPtXqqKOcWg'
+# FOK.HEROKU.COM
+GOOGLE_API_CODE = 'ABQIAAAAb0Sv2hCd8UdPqsnJb4tlxhSuHW_e0bmW98OEDUUx7DH-1gkwmhR-nrpauXqPZUkA_fl4urXeYuDCDA' 
 
 get '/map' do
 	erb open("map.html.erb","r").read()
