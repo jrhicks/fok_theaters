9178337586a6015985d3da555564d67d586d92a7
Jeffrey Hicks <jhicks@cteh.com>
2011-05-15 15:39:22 -0500

    style and copy

--- fok_theaters.rb
+++ fok_theaters.rb
@@ -6,11 +6,14 @@ require 'theaters.rb'
 # GOOGLE_API_CODE = 'ABQIAAAA9OOBC0JIUjp2D8RWTK8KehT2yXp_ZAY8_ufC3CFXhHIE1NvwkxS-XTc7wgiqy8tNe92hPtXqqKOcWg'
 # FOK.HEROKU.COM
 GOOGLE_API_CODE = 'ABQIAAAAb0Sv2hCd8UdPqsnJb4tlxhSuHW_e0bmW98OEDUUx7DH-1gkwmhR-nrpauXqPZUkA_fl4urXeYuDCDA' 
+index_erb = open("map.html.erb","r").read()
 
-get '/map' do
-	erb :map
+set :public, 'static'
+
+get '/' do
+	erb index_erb
 end
 
 get	 '/search' do
-	
+	return params[:address]
 end
