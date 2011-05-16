3af5c8429b992eb4f9408dfa7eca8e285fba7109
Jeffrey Hicks <jhicks@cteh.com>
2011-05-16 14:41:15 -0500

    retry commit

--- fok_theaters.rb
+++ fok_theaters.rb
@@ -3,39 +3,53 @@ require 'cgi'
 require 'HTTParty'
 require 'sinatra'
 require 'theaters.rb'
+require 'erb'
 
-
-# LOCAL HOST
-# GOOGLE_API_CODE = 'ABQIAAAA9OOBC0JIUjp2D8RWTK8KehT2yXp_ZAY8_ufC3CFXhHIE1NvwkxS-XTc7wgiqy8tNe92hPtXqqKOcWg'
-# FOK.HEROKU.COM
 GOOGLE_API_CODE = 'ABQIAAAAb0Sv2hCd8UdPqsnJb4tlxhSuHW_e0bmW98OEDUUx7DH-1gkwmhR-nrpauXqPZUkA_fl4urXeYuDCDA' 
-index_erb = open("map.html.erb","r").read()
+
+COUNT = 3
 
 set :public, 'static'
 
 get '/' do
-	erb index_erb
+	erb :index
 end
 
 get	 '/search' do
+	# Geocode Source Address
+	url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(params[:address])}&sensor=false"
+	response = HTTParty.get(url).parsed_response
+	
+	if response["status"]=="OK"
+		@source_lat = response["results"][0]["geometry"]["location"]["lat"].to_f
+		@source_lon = response["results"][0]["geometry"]["location"]["lng"].to_f
+	else
+		return "Unable to locate #{params[:address]}"
+	end
+
+	sorted_theaters = THEATERS.sort {|a,b| Math.sqrt((@source_lat-a[:latitude])**2+(@source_lon-a[:longitude])**2) <=> Math.sqrt((@source_lat-b[:latitude])**2+(@source_lon-b[:longitude])**2)}
+
 	sources = params[:address]
-	destinations = THEATERS.map{|t| "#{t.address}, #{t.city}  #{t.state}"}.join("|")
-    url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{sources}&destinations=#{destinations}&mode=driving&sensor=false"
-    escaped_url = CGI.escape_url(url)
-	response = HTTParty.get(escaped_url).parsed_response
-	elements = response[0]["elements"] # only one source
+	destinations = sorted_theaters.slice(0,COUNT).map{|t| "#{t[:address]}, #{t[:city]}  #{t[:state]}" }.join("|")
+    url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{CGI.escape(sources)}&destinations=#{CGI.escape(destinations)}&mode=driving&sensor=false"
+	
+    response = HTTParty.get(url).parsed_response
+	elements = response["rows"][0]["elements"] # only one source
 	
 	index = 0
-	results = THEATERS.map do |t|
+	results = sorted_theaters.slice(0,COUNT).each do |t|
 		element = elements[index]
 		index = index+1
-		t["distance_text"] = element["distance"]["text"]
-		t["distance_value"] = element["distance"]["value"]
-		t["duration_text"] = element["duration"]["text"]
-		t["duration_value"] = element["duration"]["value"]
-		t["status"] = element["status"]
+		t[:distance_text] = element["distance"]["text"]
+		t[:distance_value] = element["distance"]["value"]
+		t[:duration_text] = element["duration"]["text"]
+		t[:duration_value] = element["duration"]["value"]
+		t[:status] = element["status"]
 		end
-	return results
+		
+	@nearest = results.min {|a,b| a[:duration_value] <=> b[:duration_value]}
+	
+	erb :search
 end
 		
 	
