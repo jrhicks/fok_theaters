bfbd39fe325e9d1ac8c89f97b543fda94817097e
Jeffrey Hicks <jhicks@cteh.com>
2011-05-16 13:05:02 -0500

    search functionality

--- fok_theaters.rb
+++ fok_theaters.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
+require 'cgi'
+require 'HTTParty'
 require 'sinatra'
 require 'theaters.rb'
+
 
 # LOCAL HOST
 # GOOGLE_API_CODE = 'ABQIAAAA9OOBC0JIUjp2D8RWTK8KehT2yXp_ZAY8_ufC3CFXhHIE1NvwkxS-XTc7wgiqy8tNe92hPtXqqKOcWg'
@@ -15,5 +18,24 @@ get '/' do
 end
 
 get	 '/search' do
-	return params[:address]
+	sources = params[:address]
+	destinations = THEATERS.map{|t| "#{t.address}, #{t.city}  #{t.state}".join("|")
+    url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{sources}&destinations=#{destinations}&mode=driving&sensor=false"
+    escaped_url = CGI.escape_url(url)
+	response = HTTParty.get(escaped_url).parsed_response
+	elements = response[0]["elements"] # only one source
+	
+	index = 0
+	results = THEATERS.map do |t|
+		element = elements[index]
+		index = index+1
+		t["distance_text"] = element["distance"]["text"]
+		t["distance_value"] = element["distance"]["value"]
+		t["duration_text"] = element["duration"]["text"]
+		t["duration_value"] = element["duration"]["value"]
+		t["status"] = element["status"]
+		end
+	return results
 end
+		
+	
