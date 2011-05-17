f55980b661401478b923e06e65d4aafcc3af0c56
Jeffrey Hicks <jhicks@cteh.com>
2011-05-16 19:36:22 -0500

    replace httparty with net/http, uri, and json gems

--- fok_theaters.rb
+++ fok_theaters.rb
@@ -4,10 +4,11 @@ require 'theaters.rb'
 require 'cgi'
 require 'net/http'
 require 'uri'
+require 'json'
 
 GOOGLE_API_CODE = 'ABQIAAAAb0Sv2hCd8UdPqsnJb4tlxhSuHW_e0bmW98OEDUUx7DH-1gkwmhR-nrpauXqPZUkA_fl4urXeYuDCDA' 
 
-COUNT = 3
+COUNT = 5
 
 set :public, 'static'
 
@@ -16,12 +17,10 @@ get '/' do
 end
 
 get	 '/search' do
+
 	# Geocode Source Address
 	url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(params[:address])}&sensor=false"
-	data = Net::HTTP.get(URI.parse(url))
-	return data
-	response = HTTParty.get(url).parsed_response
-	
+	response = JSON.parse(Net::HTTP.get(URI.parse(url)))
 	if response["status"]=="OK"
 		@source_lat = response["results"][0]["geometry"]["location"]["lat"].to_f
 		@source_lon = response["results"][0]["geometry"]["location"]["lng"].to_f
@@ -29,15 +28,13 @@ get	 '/search' do
 		return "Unable to locate #{params[:address]}"
 	end
 
+	# Distance Matrix
 	sorted_theaters = THEATERS.sort {|a,b| Math.sqrt((@source_lat-a[:latitude])**2+(@source_lon-a[:longitude])**2) <=> Math.sqrt((@source_lat-b[:latitude])**2+(@source_lon-b[:longitude])**2)}
-
 	sources = params[:address]
 	destinations = sorted_theaters.slice(0,COUNT).map{|t| "#{t[:address]}, #{t[:city]}  #{t[:state]}" }.join("|")
     url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{CGI.escape(sources)}&destinations=#{CGI.escape(destinations)}&mode=driving&sensor=false"
-	
-    response = HTTParty.get(url).parsed_response
+    response = JSON.parse(Net::HTTP.get(URI.parse(url)))
 	elements = response["rows"][0]["elements"] # only one source
-	
 	index = 0
 	results = sorted_theaters.slice(0,COUNT).each do |t|
 		element = elements[index]
@@ -48,7 +45,8 @@ get	 '/search' do
 		t[:duration_value] = element["duration"]["value"]
 		t[:status] = element["status"]
 		end
-		
+	
+	# Use only the nearest
 	@nearest = results.min {|a,b| a[:duration_value] <=> b[:duration_value]}
 	
 	erb :search
